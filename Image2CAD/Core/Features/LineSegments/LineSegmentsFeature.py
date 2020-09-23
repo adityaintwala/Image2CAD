05c1663bf4274eef44dbcbf18289234b059fc7c3
Aditya Intwala <adityaintwala@yahoo.co.in>
2020-09-23 09:24:49 +0530

    Added LineSegmentsFeature

---
+++ Image2CAD/Core/Features/LineSegments/LineSegmentsFeature.py
@@ -0,0 +1,280 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: Aditya Intwala
+"""
+
+import cv2
+import numpy as np
+import Math
+import SpecialLineSegments
+from Cognition.Cognition import Cognition
+from FeatureManager import ExtractedLines
+
+from xml.etree import ElementTree as ET
+from xml.etree.ElementTree import Element, SubElement
+
+global img, threshImg, imageCorner, imageHoughLine, imageOutput, imgHeight, imgWidth, imgChannels, ArrowHeadsList,blankImg
+
+class LineSegmentsFeature():
+    global make_dir_root, timestr, DManager
+    
+    @staticmethod
+    def IsCollinear(l, ls):
+       startPtOnLine = l.OnLine(ls.startPoint)
+       endPtOnLine = l.OnLine(ls.endPoint)
+       if startPtOnLine == True and endPtOnLine == True:
+          return True
+       return False
+
+    @staticmethod
+    def CollinearLineExists(lineDict, ls):
+       lines = lineDict.keys()
+       for l in lines:
+         if LineSegmentsFeature.IsCollinear(l, ls):
+            return l
+       return None
+
+    @staticmethod
+    def GetUniqueLines(lineSegments):
+       lines = {}
+       for i in range(1, len(lineSegments)):
+         ls = lineSegments[i]
+         cl = LineSegmentsFeature.CollinearLineExists(lines, ls)
+         if None == cl:
+            temp = Math.Line2(ls.startPoint, ls.endPoint)
+            if temp.IsDegenerate() == False:
+               lines[temp] = []
+               lines[temp].append(ls)
+         else:
+             lines[cl].append(ls)
+       
+       return lines
+
+    @staticmethod
+    def DetectLineSegments(segLines):
+       lineSegmentsDetected = []
+       for sl in segLines:
+          sl.ProjectCorners()
+          sl.SortCorners()
+          sl.ProjectLineSegmentEnds()
+          sl.listCorners()
+          lineSegments = sl.DetectLineSegments()
+          lineSegmentsDetected.append(lineSegments)
+       return lineSegmentsDetected
+
+    @staticmethod
+    def DetectDimensionalLineSegments(segLines, ArrowHeadList):
+       global ArrowHeadsList
+       ArrowHeadsList = ArrowHeadList
+       lineSegmentsDetected = []
+       for sl in segLines:
+          sl.ProjectCorners()
+          sl.SortCorners()
+          sl.ProjectLineSegmentEnds()
+          lineSegments = sl.DetectDimensionalLineSegments()
+          if len(lineSegments) != 0:
+              for l in lineSegments:
+                    lineSegmentsDetected.append(l)
+       return lineSegmentsDetected
+
+    @staticmethod
+    def Initialize(image):
+        global img, imageCorner, imageHoughLine, imageOutput, imgHeight, imgWidth, imgChannels, DManager 
+        img = image
+        imgHeight, imgWidth, imgChannels = img.shape
+        imageOutput = DManager._ImageOriginal.copy()
+        imageCorner = img.copy()
+        imageHoughLine = img.copy()
+
+    @staticmethod
+    def InitializeDimLine(image):
+        global img, imageCorner, imageHoughLine, imageOutput, imgHeight, imgWidth, imgChannels
+        img = image
+        imgHeight, imgWidth, imgChannels = img.shape
+        imageOutput = img.copy()
+        imageCorner = img.copy()
+        imageHoughLine = img.copy()
+
+    
+    @staticmethod        
+    def HoughLineP(img, rho, theta, threshold, minLineLength, maxLineGap):
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        canny = cv2.Canny(gray, 50, 150, apertureSize = 3)
+
+        lines = cv2.HoughLinesP(canny, int(rho), float(theta), int(threshold), int(minLineLength), int(maxLineGap))
+        extractedLines = []
+        if None != lines.any():
+            for line in lines:
+                for x1,y1,x2,y2 in line:
+                    p1 = Math.Point2(x1,y1)
+                    p2 = Math.Point2(x2,y2)
+                    ex = ExtractedLines.ExtractedLines()
+                    ex.ExtractLine(ex,rho, theta, p1, p2)
+                    extractedLines.append(ex)
+                    cv2.line(img, (x1,y1), (x2,y2), (0,0,255), 1)
+
+ 
+        return extractedLines, img
+    
+    @staticmethod
+    def CornerShiTomasai (img, numCorners, qualityOfCorner, minElucDistance, useHarris):
+        image = img
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        
+        corners = cv2.goodFeaturesToTrack(gray, numCorners, qualityOfCorner, minElucDistance, useHarrisDetector = useHarris)
+        corners = np.int0(corners)
+
+        points = []
+        for i in corners:
+            x,y = i.ravel()
+            cv2.circle(image,(x,y), 2, (0,60,255),-1)
+            points.append(Math.Point2(x, y))
+        
+        return points,image
+    
+    @staticmethod
+    def EntityDetection():
+        global imageCorner, imageHoughLine
+        (cornerPts, cornerImageOutput) = LineSegmentsFeature.CornerShiTomasai(imageCorner, 200, 0.01, 10, True)
+        (extractedLinesP, houghlineImageOutput) = LineSegmentsFeature.HoughLineP(imageHoughLine, 1.0, 0.00872, 10, 10, 5)
+        return cornerPts, cornerImageOutput, extractedLinesP, houghlineImageOutput
+
+    @staticmethod
+    def SegmentCreator(extractedLinesP):
+
+        lineSegments = []
+        for el in extractedLinesP:
+           ls = Math.Line2(el._p1, el._p2)
+           lineSegments.append(ls)
+
+        uniqueLines = LineSegmentsFeature.GetUniqueLines(lineSegments)
+        return uniqueLines
+
+    @staticmethod
+    def SeggeratedCreator(uniqueLines):
+        segLines = []
+        for lkey in uniqueLines.keys():
+           l = lkey
+           lineSegments = uniqueLines[lkey]
+           sl = SpecialLineSegments()
+           sl._line = l;
+           sl._lineSegments = lineSegments
+           sl._cornerPoints = []
+           segLines.append(sl)
+        return segLines
+
+    @staticmethod
+    def cornerOnSegLine(cornerPts, segLines):
+        for cp in cornerPts:
+           for sl in segLines:
+              line = sl._line
+              if line.OnLine(cp) == True:
+                  sl._cornerPoints.append(cp)
+        
+    
+    @staticmethod
+    def DrawLines(detectedLineSegments):
+        global imageOutput, blankImg
+        for i in detectedLineSegments:
+            for ls in i:
+                p1 = ls.startPoint
+                p2 = ls.endPoint
+                cv2.line(imageOutput, (int(p1.x),int(p1.y)), (int(p2.x), int(p2.y)), (255,0,0), 1)  
+        return imageOutput                        
+
+    @staticmethod
+    def PlotLine(detectedLineSegments):
+        global imageOutput, img, blankImg
+        for i in detectedLineSegments:
+            ls = i._Leaders
+            for l in ls:
+                p1 = l.startPoint
+                p2 = l.endPoint
+                cv2.line(imageOutput, (int(p1.x),int(p1.y)), (int(p2.x), int(p2.y)), (255,0,0), 1)
+        return imageOutput
+
+    @staticmethod
+    def DisplayOutputs():
+        global imageCorner, imageHoughLine, imageOutput, make_dir_root,blankImg
+        cv2.imwrite(make_dir_root + "/Line_Extraction_Output.png",imageOutput)
+         
+
+    @staticmethod
+    def Make_Directory(makedir_root):
+        global make_dir_root
+        make_dir_root = makedir_root
+
+    @staticmethod
+    def Remove_Empty(detectedLineSegments):
+        Segments = []
+        for ls in detectedLineSegments:
+            if len(ls) !=0 :
+                for l in ls:
+                    Segments.append(l)
+        return Segments
+
+    @staticmethod
+    def Remove_EmptyLS(detectedLineSegments):
+        Segments = []
+        for ls in detectedLineSegments:
+            if len(ls) !=0 :
+                Segments.append(ls)
+        return Segments
+
+       
+    @staticmethod
+    def Detect(Feature_Manager):
+        global make_dir_root, timestr, DManager
+        DManager = Feature_Manager
+        make_dir_root = Feature_Manager._RootDirectory
+        image = Feature_Manager._ImageCleaned.copy()
+        
+        LineSegmentsFeature.Initialize(image)
+        
+        cornerPts, cornerImageOutput, extractedLinesP, houghlineImageOutput = LineSegmentsFeature.EntityDetection()
+        
+        uniqueLines = LineSegmentsFeature.SegmentCreator(extractedLinesP)
+        
+        segLines = LineSegmentsFeature.SeggeratedCreator(uniqueLines)
+        
+        LineSegmentsFeature.cornerOnSegLine(cornerPts, segLines)
+        
+        detectedLineSegments = LineSegmentsFeature.DetectLineSegments(segLines)
+        
+        Segments = Cognition.MergeLineSegments(detectedLineSegments)
+        
+        LineImg = LineSegmentsFeature.DrawLines(Segments)
+        
+        LineSegmentsFeature.DisplayOutputs()
+        
+        
+        return Segments, LineImg
+    
+    
+    @staticmethod
+    def Dump(make_dir_root, segments):
+        Root = Element("Root")
+        Extracted_Line_Segments = SubElement(Root, "Extracted_Line_Segments")
+        i = 0
+        for item in segments:
+            Extracted_Segment = SubElement(Extracted_Line_Segments, "Extracted_Segment")
+            Extracted_Segment.text = str(i)
+            Start_Point = SubElement(Extracted_Segment, "Start_Point")
+            Start_Point_x = SubElement(Start_Point, "x")
+            Start_Point_x.text = str(item.startPoint.x)
+            Start_Point_y = SubElement(Start_Point, "y")
+            Start_Point_y.text = str(item.startPoint.y)
+            End_Point = SubElement(Extracted_Segment, "End_Point")
+            End_Point_x = SubElement(End_Point, "x")
+            End_Point_x.text = str(item.endPoint.x)
+            End_Point_y = SubElement(End_Point, "y")
+            End_Point_y.text = str(item.endPoint.y)
+            i += 1
+           
+        tree = ET.ElementTree(Root)
+        tree.write(make_dir_root +"/Line_Segment_Extraction.xml")
+
+
+        
+        
---
+++ Image2CAD/Core/Features/LineSegments/SpecialLineSegments.py
@@ -0,0 +1,354 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: Aditya Intwala
+"""
+
+import cv2
+from math import fabs
+import numpy as np
+import Math
+from FeatureManager import DimensionalLines
+
+global img, threshImg, imageCorner, imageHoughLine, imageOutput, imgHeight, imgWidth, imgChannels, ArrowHeadsList,blankImg
+
+class SpecialLineSegments:
+
+    def __init__(self):
+        self._p1 = Math.Point2(0,0)
+        self._p2 = Math.Point2(0,0)
+        self._line = Math.Line2(self._p1, self._p2)
+        self._lineSegments = []
+        self._cornerPoints = []
+
+    def __repr__(self):
+        return "".join(["SpecialLineSegments[ (Line = ", str(self._line), "),(LineSegments = ", str(self._lineSegments), "),(CornerPoints = ", str(self._cornerPoints), ")]"])
+
+    def ProjectCorners(self):
+       corners = self._cornerPoints
+       projCorners = []
+       for cp in corners:
+          pcp = Math.MathUtils.ProjectToLine2(self._line.startPoint, self._line.endPoint, cp)
+          projCorners.append(pcp)
+       self._cornerPoints = projCorners
+
+    def ProjectLineSegmentEnds(self):
+       projLineSegments = []
+       for ls in self._lineSegments:
+          lss = ls.startPoint
+          lse = ls.endPoint
+          lssp = Math.MathUtils.ProjectToLine2(self._line.startPoint, self._line.endPoint, lss)
+          lsep = Math.MathUtils.ProjectToLine2(self._line.startPoint, self._line.endPoint, lse)
+          lsp = Math.Line2(lssp, lsep)
+          projLineSegments.append(lsp)
+       self._lineSegments = projLineSegments
+
+    def SortCorners(self):
+       sortedCornersDict = {}
+       corners = self._cornerPoints
+       for cp in corners:
+         u = SpecialLineSegments.GetUParam(cp,self)                  
+         sortedCornersDict[u] = cp
+       keys = sortedCornersDict.keys()
+       sortedKeys = sorted(keys)
+       sortedCorners = []
+       for i in range(0, len(sortedKeys)):
+          key = sortedKeys[i]
+          corner = sortedCornersDict[key]
+          sortedCorners.append(corner)
+       self._cornerPoints = sortedCorners
+       self.SetEnds()
+
+    def SetEnds(self):
+       points = []
+       numCorners = len(self._cornerPoints)
+       if numCorners >= 1:
+            lsp1, lsp2 = SpecialLineSegments.SeggregatePoints(self._lineSegments, self._line)
+            lsp1 = Math.MathUtils.ProjectToLine2(self._line.startPoint, self._line.endPoint, lsp1)
+            lsp2 = Math.MathUtils.ProjectToLine2(self._line.startPoint, self._line.endPoint, lsp2)
+            cs = self._cornerPoints[0]
+            ce = self._cornerPoints[numCorners - 1]
+            points.append(lsp1)
+            points.append(lsp2)
+            points.append(cs)
+            points.append(ce)
+            sortedEndPts = SpecialLineSegments.sortPoints(points, self._line)
+            numPts = len(sortedEndPts)
+            s = sortedEndPts[0]
+            e = sortedEndPts[numPts - 1]
+            line = Math.Line2(s, e)
+            self._line = line
+    
+    def listCorners(self):
+        corners = self._cornerPoints
+        listCorners = []
+        line = self._line
+        s = line.startPoint
+        e = line.endPoint
+        listCorners.append(s)
+        listCorners.append(e)
+        for cp in corners:
+            listCorners.append(cp)
+
+        self._cornerPoints = listCorners
+        sortedpoints = SpecialLineSegments.sortPoints(self._cornerPoints, self._line)
+        self._cornerPoints = sortedpoints
+
+
+    @staticmethod
+    def sortPoints(points, line):
+        sortedPointsDict = {}
+        for p in points:
+          u = SpecialLineSegments.GetUParamLine(p,line)                  #
+          sortedPointsDict[u] = p
+        keys = sortedPointsDict.keys()
+        sortedKeys = sorted(keys)
+        sortedPoints = []
+        for i in range(0, len(sortedKeys)):
+           key = sortedKeys[i]
+           point = sortedPointsDict[key]
+           sortedPoints.append(point)
+        return sortedPoints
+
+    @staticmethod
+    def SeggregatePoints(linesegments, line):
+        points = []
+        for ls in linesegments:
+            sPt = ls.startPoint
+            ePt = ls.endPoint
+            points.append(sPt)
+            points.append(ePt)
+        sortedSegPoints = SpecialLineSegments.sortPoints(points, line)
+        p1 = sortedSegPoints[0]
+        p2 = sortedSegPoints[(len(sortedSegPoints) - 1)]
+        return p1, p2
+    
+    def CheckPixelsInVicinity(self,x, y, threshImg):
+        imgHeight = threshImg.shape[0]
+        imgWidth = threshImg.shape[1]
+        scanRange = 3               
+        if x < (imgWidth - scanRange) and y < (imgHeight - scanRange):
+            for i in range(-scanRange, scanRange):
+                for j in range(-scanRange, scanRange):
+                    xj = x + j
+                    yi = y + i
+                    if threshImg[yi, xj] == 0:
+                        return True
+        return False
+
+    def FindPercentageOfPointsOnLine(self, pointsOnLineBtnCorners):
+      global threshImg
+      TotalPixel = len(pointsOnLineBtnCorners)
+      NonZeroPixel = 0
+      for i in pointsOnLineBtnCorners:
+          pixelPresent = self.CheckPixelsInVicinity(i[0], i[1], threshImg)
+          if pixelPresent == True:
+              NonZeroPixel += 1
+              
+      percent = float((100 * NonZeroPixel)/ TotalPixel)
+      return percent
+
+    @staticmethod
+    def GetUParam(c1, sl):
+       line = sl._line                                                    
+       startpt = line.startPoint
+       endpt = line.endPoint
+       denominator = fabs(endpt.x - startpt.x)
+       tolerance = 1.0
+       U = 0
+       if denominator < tolerance :
+           U = float((c1.y - startpt.y) / (endpt.y - startpt.y))
+       else:
+           U = float((c1.x - startpt.x) / (endpt.x - startpt.x))
+       return U
+
+    @staticmethod
+    def GetUParamLine(c1, sl):
+       line = sl                                                    
+       startpt = line.startPoint
+       endpt = line.endPoint
+       denominator = fabs(endpt.x - startpt.x)
+       tolerance = 1.0
+       U = 0
+       if denominator < tolerance :
+           U = float((c1.y - startpt.y) / (endpt.y - startpt.y))
+       else:
+           U = float((c1.x - startpt.x) / (endpt.x - startpt.x))
+       return U
+
+    @staticmethod
+    def PixelScanner(p1, p2, img):
+        
+        height = img.shape[0]
+        width = img.shape[1]
+        p1x = p1.x
+        p2x = p2.x
+        p1y = p1.y
+        p2y = p2.y
+
+        horizontal_projection = p2x - p1x
+        absolute_horizontal_projection = fabs(horizontal_projection)
+        absolute_horizontal_projection = round(absolute_horizontal_projection)
+        vertical_projection = p2y - p1y
+        absolute_vertical_projection = fabs(vertical_projection)
+        absolute_vertical_projection = round(absolute_vertical_projection)
+        number = np.maximum(absolute_vertical_projection , absolute_horizontal_projection)
+        line_pixel_array = np.empty(shape=(np.maximum(absolute_vertical_projection , absolute_horizontal_projection) , 3), dtype = np.float32)
+        line_pixel_array.fill(np.nan)
+        
+        negative_Y = p1y > p2y
+        negative_X = p1x > p2x
+        if p1y == p2y: 
+            line_pixel_array[ : , 1 ] = p1y
+            if negative_X:
+                line_pixel_array[ : , 0 ] = np.arange(p1x - 1 , p1x - absolute_horizontal_projection - 1 , -1)
+            else:
+                line_pixel_array[ : , 0 ] = np.linspace(p1x + 1 , p1x + absolute_horizontal_projection + 1 , number)
+        elif p1x == p2x: 
+            line_pixel_array[ : , 0 ] = p1x
+            if negative_Y:
+                line_pixel_array[ : , 1 ] = np.arange(p1y - 1 , p1y - absolute_vertical_projection - 1 , -1)
+            else:
+                line_pixel_array[ : , 1 ] = np.linspace(p1y + 1 , p1y + absolute_vertical_projection + 1 , number)
+        else: 
+            large_Slope = absolute_vertical_projection > absolute_horizontal_projection
+            if large_Slope:
+                slope = horizontal_projection.astype(np.float32) / vertical_projection.astype(np.float32)
+                if negative_Y:
+                    line_pixel_array[ : , 1 ] = np.arange(p1y - 1 , p1y - absolute_vertical_projection - 1 , -1)
+                else:
+                    line_pixel_array[ : , 1 ] = np.linspace(p1y + 1 , p1y + absolute_vertical_projection + 1, number)
+                line_pixel_array[ : , 0 ] = (slope * (line_pixel_array[ : , 1] - p1y)).astype(np.int) + p1x         #Based on eq of line between two points
+            else:
+                slope = vertical_projection.astype(np.float32) / horizontal_projection.astype(np.float32)
+                if negative_X:
+                    line_pixel_array[ : , 0 ] = np.arange(p1x - 1 , p1x - absolute_horizontal_projection - 1 , -1)
+                else:
+                    line_pixel_array[ : , 0] = np.linspace(p1x + 1, p1x + absolute_horizontal_projection + 1, num = number)
+                line_pixel_array[ : , 1] = (slope * (line_pixel_array[ : , 0] - p1x)).astype(np.int) + p1y          #Based on eq of line between two points
+
+        X_Coordinate_Array = line_pixel_array[ : , 0]
+        Y_Coordinate_Array = line_pixel_array[ : , 1]
+        line_pixel_array = line_pixel_array[(X_Coordinate_Array >= 0) & (Y_Coordinate_Array >= 0) & (X_Coordinate_Array < width) & (Y_Coordinate_Array < height)]
+
+        line_pixel_array[:,2] = img[line_pixel_array[:,1].astype(np.uint),line_pixel_array[:,0].astype(np.uint)]
+
+        return line_pixel_array
+
+    @staticmethod
+    def ReturnArrowHeads(center):
+        global ArrowHeadsList
+    
+        for a in ArrowHeadsList:
+            arrow_center = a._ArrowCenter
+            if fabs(int(arrow_center.x - center.x)) <= 5 and fabs(int(arrow_center.y - center.y)) <= 5:     # <=3 changed on 10-3-16 due to change in onLine threshold in Line DT Math3D
+                a._ArrowCenter = center
+                return a
+        return None
+
+    @staticmethod
+    def CheckArrowOverlap(arrowHead):
+        global ArrowHeadsList
+
+        for a in ArrowHeadsList:
+            if fabs(arrowHead._ArrowCenter.x - a._ArrowCenter.x) > 1 or fabs(arrowHead._ArrowCenter.y != a._ArrowCenter.y) > 1:
+                overlap = Cognition.CheckOverlapByLineSegment(arrowHead._BoundingBoxP1, arrowHead._BoundingBoxP2, a._BoundingBoxP1, a._BoundingBoxP2)
+                if overlap == True:
+                    return True
+        return False
+
+    @staticmethod
+    def CheckArrowOverlapBetweenTwo(a1, a2):
+        if a1._ArrowCenter.x != a2._ArrowCenter.x or a1._ArrowCenter.y != a2._ArrowCenter.y:
+                overlap = Cognition.CheckOverlapByLineSegment(a1._BoundingBoxP1, a1._BoundingBoxP2, a2._BoundingBoxP1, a2._BoundingBoxP2)
+                if overlap == True:
+                    return True
+        return False
+
+    def DetectDimensionalLineSegments(self):
+       global img, threshImg
+       gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+       ret, threshImg = cv2.threshold(gray_img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+       numCorners = len(self._cornerPoints)
+       ExtractedDimensionalLine = []
+     
+       if numCorners == 1:
+              c1 = self._cornerPoints[0]
+              c2 = self._line.startPoint
+              c3 = self._line.endPoint
+              for c in (c2,c3):
+                Dimensional_LS = []
+                Dimensional_AH = []
+                if int(fabs(c1.x - c.x)) >= 3 or int(fabs(c1.y - c.y)) >= 3:
+                    pointsOnLineBtnCorners = SpecialLineSegments.PixelScanner(c1, c, threshImg)
+                    onlinePointsPercentage = self.FindPercentageOfPointsOnLine(pointsOnLineBtnCorners)
+                    if onlinePointsPercentage > 90:
+                        newlineSegm = Math.Line2(c1,c)
+                        if int(newlineSegm.Length()) > 8:                       #updated on 29-2-16
+                            arrowHead = SpecialLineSegments.ReturnArrowHeads(c1)
+                            if arrowHead is None:
+                                continue
+                            overlap = SpecialLineSegments.CheckArrowOverlap(arrowHead)       #Added on 29-2-16(to reduce Line between two arrowheads connection)
+                            if overlap == False:                                        #Added on 29-2-16(to reduce Line between two arrowheads connection)
+                                Dimensional_AH.append(arrowHead)
+                                Dimensional_LS.append(newlineSegm)
+                                DL = DimensionalLines()
+                                DL.ExtractDimensionalLine(DL, Dimensional_AH, Dimensional_LS)
+                                ExtractedDimensionalLine.append(DL)
+        
+       else:
+             for i in range(0, numCorners-1):
+                    Dimensional_LS = []
+                    Dimensional_AH = []
+                    c1 = self._cornerPoints[i]
+                    c2 = self._cornerPoints[i+1]
+                    if int(fabs(c1.x - c2.x)) >= 3 or int(fabs(c1.y - c2.y)) >= 3 :                
+                        pointsOnLineBtnCorners = SpecialLineSegments.PixelScanner(c1, c2, threshImg)
+                        onlinePointsPercentage = self.FindPercentageOfPointsOnLine(pointsOnLineBtnCorners)
+                        if onlinePointsPercentage > 90:
+                            newlineSegm = Math.Line2(c1,c2)
+                            if int(newlineSegm.Length()) > 8:                   #updated on 29-2-16
+                                arrowHead1 = SpecialLineSegments.ReturnArrowHeads(c1)
+                                arrowHead2 = SpecialLineSegments.ReturnArrowHeads(c2)
+                                if arrowHead1 is None or arrowHead2 is None:
+                                    continue
+                                overlap = SpecialLineSegments.CheckArrowOverlapBetweenTwo(arrowHead1, arrowHead2)    #Added on 29-2-16(to reduce Line between two arrowheads connection)
+                                if overlap == False:                                                            #Added on 29-2-16(to reduce Line between two arrowheads connection)
+                                    Dimensional_LS.append(newlineSegm)
+                                    Dimensional_AH.append(arrowHead1)
+                                    Dimensional_AH.append(arrowHead2)
+                                    DL = DimensionalLines()
+                                    DL.ExtractDimensionalLine(DL, Dimensional_AH, Dimensional_LS)
+                                    ExtractedDimensionalLine.append(DL)
+       return ExtractedDimensionalLine
+
+    def DetectLineSegments(self):
+       detectedLineSegments = []
+       global img, threshImg
+       gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+       ret, threshImg = cv2.threshold(gray_img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+
+       numCorners = len(self._cornerPoints)
+       if numCorners == 1:
+              c1 = self._line.startPoint
+              c2 = self._line.endPoint
+              pointsOnLineBtnCorners = SpecialLineSegments.PixelScanner(c1, c2, threshImg)
+              onlinePointsPercentage = self.FindPercentageOfPointsOnLine(pointsOnLineBtnCorners)
+              if onlinePointsPercentage > 90:
+                    newlineSegm = Math.Line2(c1,c2)
+                    if int(newlineSegm.Length()) > 6:           #Updated on 18-2-16
+                        detectedLineSegments.append(newlineSegm)
+       else:
+
+             for i in range(0, numCorners-1):
+                
+                c1 = self._cornerPoints[i]
+                c2 = self._cornerPoints[i+1]
+                if int(fabs(c1.x - c2.x)) >= 3 or int(fabs(c1.y - c2.y)) >= 3 :                
+                    pointsOnLineBtnCorners = SpecialLineSegments.PixelScanner(c1, c2, threshImg)
+                    onlinePointsPercentage = self.FindPercentageOfPointsOnLine(pointsOnLineBtnCorners)
+                    if onlinePointsPercentage > 90:
+                        newlineSegm = Math.Line2(c1,c2)
+                        if int(newlineSegm.Length()) > 6:           #Updated on 18-2-16
+                            detectedLineSegments.append(newlineSegm)
+       return detectedLineSegments
