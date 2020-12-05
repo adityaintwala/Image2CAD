9363f5d03c66b9c462dfd4e1ee6d37a780f7227b
Aditya Intwala <adityaintwala@yahoo.co.in>
2020-12-05 13:25:39 +0530

    Added DXFWriter Uses ezdxf for now.

---
+++ Image2CAD/Core/Utils/DXFWriter.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: Aditya Intwala
+"""
+
+import os
+import ezdxf
+
+class DXFWriter:
+
+       @staticmethod
+       def Write(Feature_Manager):
+           dxfFileName = os.path.splitext(os.path.basename(Feature_Manager._ImagePath))[0]
+           dxfFilePath = Feature_Manager._RootDirectory + "/" + dxfFileName +".dxf"
+           
+           drawing = ezdxf.new('R2010')
+           msp = drawing.modelspace()
+           
+           for L in Feature_Manager._DetectedLine:
+               if(L.endPoint == L.startPoint):
+                   continue
+               msp.add_line((L.startPoint.x, L.startPoint.y), (L.endPoint.x, L.endPoint.y))
+               offset = 4
+               dim = msp.add_aligned_dim(p1=(L.startPoint.x, L.startPoint.y), p2=(L.endPoint.x, L.endPoint.y), distance=offset) #, text='DimText'
+               dim.set_arrows(blk=ezdxf.ARROWS.closed_filled, size=1)
+               dim.render()
+               
+           for C in Feature_Manager._DetectedCircle:
+               msp.add_circle(center=(C._centre.x, C._centre.y), radius=C._radius)
+               dim = msp.add_radius_dim(center=(C._centre.x, C._centre.y), radius=C._radius, angle=45, dimstyle='EZ_RADIUS')
+               dim.render()
+           
+           drawing.saveas(dxfFilePath)
